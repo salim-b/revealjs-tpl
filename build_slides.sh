4518623df5cc78d2ce994a3e7984411d720d4e06
Salim B <salim@posteo.de>
2020-10-12 14:59:56 +0200

    improve build and deploy scripts:
    - make them work from within Flatpak sandboxing (using `flatpak-spawn --host`)
    - make deploy script config more convenient

--- build_slides.sh
+++ build_slides.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-pandoc "slides.md" \
-       --from=markdown+emoji \
-       --to=revealjs \
-       --incremental \
-       --standalone \
-       --template=assets/pandoc_template \
-       --output=index.html \
 
+cmd='pandoc --from=markdown+emoji --to=revealjs --incremental --standalone --template=assets/pandoc_template --output=index.html slides.md'
+
+if [ ! -z $FLATPAK_ID ]; then
+  flatpak-spawn --host $cmd
+else
+  $cmd
+fi
--- deploy_slides.sh
+++ deploy_slides.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
-pandoc "slides.md" \
-       --from=markdown+emoji \
-       --to=revealjs \
-       --incremental \
-       --standalone \
-       --template=assets/pandoc_template \
-       --output=index.html \
 
+# CONFIG
+repo_path="/PATH/TO/YOUR/REPO/USERNAME.gitlab.io/"
+slideshow_name="SLIDESHOW"
 
-cp index.html "/PATH/TO/YOUR/REPO/USERNAME.gitlab.io/public/slides/SLIDESHOW/"
-cp -R assets/ "/PATH/TO/YOUR/REPO/USERNAME.gitlab.io/public/slides/SLIDESHOW/"
+build_cmd='pandoc --from=markdown+emoji --to=revealjs --incremental --standalone --template=assets/pandoc_template --output=index.html slides.md'
 
-cd "/PATH/TO/YOUR/REPO/USERNAME.gitlab.io/"
-git add public/slides/SLIDESHOW/*
-git commit -m "update SLIDESHOW slides"
+if [ ! -z $FLATPAK_ID ]; then
+  flatpak-spawn --host $cmd
+else
+  $cmd
+fi
+
+cp index.html "${repo_path}public/slides/${slideshow_name}/"
+cp -R assets/ "${repo_path}public/slides/${slideshow_name}/"
+
+cd "${repo_path}"
+git add "public/slides/${slideshow_name}/*"
+git commit -m "update ${slideshow_name} slides"
 git push
