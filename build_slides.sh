c4b599e4ef82a3476e50c93f3649674320572a20
Salim B <salim@posteo.de>
2021-08-22 12:35:40 +0200

    set explicit `slide-level` opt

--- build_slides.sh
+++ build_slides.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cmd='pandoc --from=markdown+emoji --to=revealjs --incremental --standalone --template=assets/pandoc_template --output=index.html slides.md'
+cmd='pandoc --from=markdown+emoji --to=revealjs --incremental --slide-level=2 --standalone --template=assets/pandoc_template --output=index.html slides.md'
 
 if [ ! -z $FLATPAK_ID ]; then
   flatpak-spawn --host $cmd
